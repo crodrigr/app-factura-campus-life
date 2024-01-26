b4fb7caa4e3e08889969bcd29181eec9bb9965fb
crodrigr <crodrigr@gmail.com>
2024-01-25 20:12:09 -0500

    listar factura

--- src/main/java/com/campusland/services/ServiceFactura.java
+++ src/main/java/com/campusland/services/ServiceFactura.java
@@ -1,13 +1,14 @@
-package com.campusland.respository;
+package com.campusland.services;
 
 import java.util.List;
 
 import com.campusland.respository.models.Factura;
 
-public interface CrudRepositoryFactura {
+public interface ServiceFactura {
 
     List<Factura> listar();
 
     void crear(Factura factura);
+
     
 }
